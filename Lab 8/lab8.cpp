9c1aca69d877c48d48fc1c05d084c940fcacd1f7
Skylar Robinson <skylardawn150@gmail.com>
2024-09-25 20:07:28 -0700

    Initial commit

---
+++ Lab 8/lab8.cpp
@@ -0,0 +1,31 @@
+/*******************************
+
+COMSCI 210 | Lab 8 | Skylar Robinson
+
+Adds two user entered integers together
+
+********************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int one, two;
+
+	//get int 1
+	cout << "Enter the first number: ";
+	cin >> one;
+
+	//get int 2
+	cout << "Enter the second number: ";
+	cin >> two;
+
+	//display sum
+	cout << "Sum: " << one + two;
+
+	return 0;
+}
+
+
+

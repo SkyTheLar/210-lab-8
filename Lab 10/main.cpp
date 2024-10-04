73bf5d869fdeda1e482ded9a4fe87293110808a6
Skylar Robinson <skylardawn150@gmail.com>
2024-10-04 15:07:21 -0700

    Commit 1

---
+++ Lab 10/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Adding 2 and 3:\n";
+
+	int num1 = 2;
+	int num2 = 3;
+
+	cout << "2 plus 3 is " << num1 + num2;
+
+    return 0;
+}
